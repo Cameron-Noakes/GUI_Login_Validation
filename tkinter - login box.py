f92a734d928b8cc129c6f635a5fd43b3365414b7
Cameron Noakes <47068266+Cameron-Noakes@users.noreply.github.com>
2021-09-27 11:32:15 +0100

    version 1 of login gui box for authentication mechanisms

---
+++ tkinter - login box.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from functools import partial
+
+def validateLogin(username, password):
+    print("username entered :", username.get())
+    print("password entered :", password.get())
+    #changes the background to show a successful login
+    tkWindow.configure(bg="black")
+    return
+
+#window
+tkWindow = Tk()  
+tkWindow.geometry('250x100')  
+tkWindow.title('Login Form AllAboutToys')
+
+#username label and text entry box
+usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
+username = StringVar()
+usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)  
+
+#password label and password entry box
+passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)  
+password = StringVar()
+passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)  
+
+validateLogin = partial(validateLogin, username, password)
+
+#login button
+loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)
+
+
+
+
+
+
+
+
+
+
+tkWindow.mainloop()
